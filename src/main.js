7616dd7a9c680e740ce45d56919280a21dfa7600
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-11-22 22:23:09 +0100

    Add code to install component for publishing

--- src/main.js
+++ src/main.js
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import './style.css'
 import CompoundInterestCalculator from "./components/CompoundInterestCalculator.vue";
 
+function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component("v-compound-interest-calculator", CompoundInterestCalculator);
+}
+
+const plugin = {
+    install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+    GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+    GlobalVue = global.vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+CompoundInterestCalculator.install = install;
+
 createApp(CompoundInterestCalculator).mount('#app')
